232d4504da9297607dc8a4b761c1df685043404c
BraxWong <ghostenjob@gmail.com>
2022-06-02 11:20:09 +0800

    Switching from an alert to a p tag to show the results

--- HTML:CSS/JS/BMI.js
+++ HTML:CSS/JS/BMI.js
@@ -3,5 +3,11 @@ function Calculate() {
     var weight = document.getElementById("Weight").value
     var height = document.getElementById("Height").value
     var BMI = weight/(height*height/10000)
-    alert("Your BMI is " + BMI)
+    if(BMI > 18.5 && BMI < 24.9){
+        document.getElementById("Results").innerHTML="Your BMI is " + BMI + ". You are healthy!"
+    }
+    else{
+        document.getElementById("Results").innerHTML="Your BMI is " + BMI + ". You are not healthy!"
+    }
+    
 }
