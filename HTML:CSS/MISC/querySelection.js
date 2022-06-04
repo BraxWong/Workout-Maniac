c981b00ef1811e45db166daf276d9039d7414e10
BraxWong <ghostenjob@gmail.com>
2022-06-04 13:51:10 +0800

    Explain what querySelection and querySelectionAll can be used

---
+++ HTML:CSS/MISC/querySelection.js
@@ -0,0 +1,9 @@
+//<p>Hi<p>
+//<ul>
+//   <li>hi</li>
+//</ul>
+
+
+
+// document.querySelector("p") == document.getElementByID("p").value
+// document.querySelectorAll('ul li')   //This will grab all the items from the unordered or ordered lists
