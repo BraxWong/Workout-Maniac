da227d3269c921d18e8f4ce4a737aaab1d955ee5
BraxWong <ghostenjob@gmail.com>
2022-06-05 00:22:01 +0800

    Switch to querySelector since its shorter

--- HTML:CSS/JS/BMI.js
+++ HTML:CSS/JS/BMI.js
@@ -1,7 +1,7 @@
 
 function Calculate() {
-    var weight = document.getElementById("Weight").value
-    var height = document.getElementById("Height").value
+    var weight = document.querySelector("Weight")
+    var height = document.querySelector("Height")
     var BMI = weight/(height*height/10000)
     if(BMI > 18.5 && BMI < 24.9){
         document.getElementById("Results").innerHTML="Your BMI is " + BMI + ". You are healthy!"
@@ -12,8 +12,8 @@ function Calculate() {
     
 }
 function Calculate_BFP() {
-    var BMI = document.getElementById("BMI").value
-    var age = document.getElementById("Age").value
+    var BMI = document.querySelector("BMI")
+    var age = document.querySelector("Age")
     var gender = document.getElementById("Gender").value.toUpperCase()
     if(gender == 'F'){
         var BFP = (1.20 * BMI) + (0.23 * age) - 5.4
