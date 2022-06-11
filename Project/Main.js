eee2c82614c7c106576ce7495f1ff0a906af7360
BraxWong <ghostenjob@gmail.com>
2022-06-11 22:53:24 +0800

    First attempt on project 1 completed.

--- Project/Main.js
+++ Project/Main.js
@@ -1,6 +1,28 @@
 function MainComponents() {
     return (
-        <h1>Main component</h1>
+        <div className="body_div">
+            <br></br><br></br>
+            <h1 className ="header">Fun facts about React</h1>
+            <br></br>
+            <ul className="list">
+            <li>
+                Was first released in 2013
+            </li>
+            <li>
+                Was originally created by Jordan Walke
+            </li>
+            <li>
+                Has well over 100K stars on Github
+            </li>
+            <li>
+                Is maintained by Facebook
+            </li>
+            <li>
+                Powers thousands of enterprise apps, including mobile apps
+            </li>
+        </ul>
+        </div>
+        
     )
 }
 function NavBar() {
